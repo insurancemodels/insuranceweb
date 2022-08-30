1552a640af16d935929cef6bf889f8f16433eb35
VernonNabibya <vernonnabibya@gmail.com>
2022-08-30 06:08:12 +0300

    Update insurance_fraud_detection.py

--- insurance_fraud_detection.py
+++ insurance_fraud_detection.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 
 import joblib
-from sklearn.model_selection import train_test_split
+
 import warnings
 import streamlit as st
 warnings.filterwarnings('ignore')
